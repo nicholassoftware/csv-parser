7be2f04f4172c3c5be1dbeedbbc3753254f1475f
nicholassoftware <nick@nicholassoftware.com>
2023-10-12 13:21:10 -0400

    ADDED: api routes of csv file uploads

---
+++ backend-csv-parser/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const { upload } = require("./services/csv-upload");
+const { CsvStringParser, validateCSV } = require("./services/csv-parse");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/", (req, res) => {
+  res.send("Hello CSV-Parser API");
+});
+
+app.post("/api/csv-parser", upload, async (req, res) => {
+  try {
+    const date = req.body.date;
+    const vendorName = req.body.vendorName;
+    if (!date || !vendorName || !req.file) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+    const fileContent = req.file?.buffer;
+    const data = await CsvStringParser(fileContent);
+    if (data) {
+      const { validData, errors } = validateCSV(data);
+      if (errors.length > 0) {
+        return res.status(400).json({ errors });
+      }
+      return res.status(200).json({ date, vendorName, validData });
+    }
+  } catch (error) {
+    return res.status(400).json({ errors });
+  }
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
