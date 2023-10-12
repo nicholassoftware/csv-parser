e61b0a401121157bdf14d197fa51a7e2bb1a6870
nicholassoftware <nick@nicholassoftware.com>
2023-10-12 15:19:32 -0400

    added vendor schema

---
+++ backend-csv-parser/Models/Vendor.js
@@ -0,0 +1,17 @@
+// const mongoose = require('mongoose');
+
+// const validDataSchema = new mongoose.Schema({
+//   modelNumber: String,
+//   unitPrice: Number,
+//   quantity: Number,
+// });
+
+// const vendorSchema = new mongoose.Schema({
+//   date: Date,
+//   vendorName: String,
+//   validData: [validDataSchema],
+// });
+
+// const Vendors = mongoose.model('Vendors', vendorSchema);
+
+// module.exports = Vendors;
