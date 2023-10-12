b69439f69e8a1bb434e1c42ba8b6b2661cf4cf34
nicholassoftware <nick@nicholassoftware.com>
2023-10-12 15:19:32 -0400

    added vendor schema

--- backend-csv-parser/server.js
+++ backend-csv-parser/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+// const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 
 const { upload } = require("./services/csv-upload");
 const { CsvStringParser, validateCSV } = require("./services/csv-parse");
+// const Vendors = require("./Models/Vendor");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -32,12 +34,32 @@ app.post("/api/csv-parser", upload, async (req, res) => {
       if (errors.length > 0) {
         return res.status(400).json({ errors });
       }
+      // Create A database schema to set data into data base
+      // const Vendors = new Vendors({ date, vendorName, validData })
+      // Vendors.save().then(() => {
+      //   return res.status(200).json({ message: "Vendors successfully validate and register" });
+      // })
       return res.status(200).json({ date, vendorName, validData });
     }
   } catch (error) {
     return res.status(400).json({ errors });
   }
 });
+
+// const mongo_uri = process.env.MONGO_URI; 
+
+// mongoose
+//   .connect(mongo_uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Successfully connected to MongoDB");
+//     // Start your application or define further actions here
+//   })
+//   .catch((error) => {
+//     console.error("MongoDB connection error:", error.message);
+//   });
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
